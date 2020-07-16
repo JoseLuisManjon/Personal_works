f63c6d3f43f16f722011cf9cdc60bfa55f802fa6
JoseLuisManjon <presimanjon@yahoo.es>
2020-07-17 01:36:56 +0200

    Added Cars and Fictius name exercises

--- week5/day2/exercises/python/Module_decoratorJM.py
+++ week5/day2/exercises/python/Module_decoratorJM.py
@@ -1,15 +1,17 @@
+import pandas as pd
 from functools import wraps
 
-def prepost(key_url):
+def prepost(*args, **kwargs):
     def inner(function):
         @wraps(function)
         def wrapper(*a, **k): 
             # antes
-            if key_url:
-                df = pd.read_csv('http://winterolympicsmedals.com/medals.csv')
+            if 'key_url' in kwargs.keys():
+                df = pd.read_csv(kwargs['key_url'])
             retval = function(*a, **k)  
             # después
             df.hist()
+            print("done")
             return retval
         return wrapper
     return inner
@@ -19,4 +21,6 @@ def _f_protected():
     l1 = [elem for elem in range(0,16)]
     c = list(filter(lambda x: True if x > 5 else False, l1))
     return c
+
 _f_protected()
+
